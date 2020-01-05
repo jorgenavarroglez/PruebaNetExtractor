3389d68faf4ace4b6db43b33b206958f98a3fd0a
Unknown <jng0020@alu.ubu.es>
2020-01-05 14:57:08 +0100

    cambio menor

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
+#from src import personaje
+#from tst import TestUnitarios
 from src import Controlador
 import unittest
+import os
 
 #runner = unittest.TextTestRunner()
 #result = runner.run(unittest.makeSuite(testUnitarios.testUnitarios))
-
-Controlador.app.run(threaded=True, debug=True)
+port = int(os.environ.get('PORT', 5000))
+Controlador.app.run(threaded=True, host='0.0.0.0', port=port)
 
